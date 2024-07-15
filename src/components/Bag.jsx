c4b0b50ce563000990df7201e77cc2046fb0f8a7
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-16 01:24:58 +0200

    Dashboard page setup

---
+++ src/components/Bag.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bag = () => {
+    return (
+        <>
+        {/* Palesa's section */}
+         Bag section
+        </>
+    );
+};
+
+export default Bag;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <>
+        {/* Kgomotso's section */}
+            Sidebar section
+        </>
+    );
+};
+
+export default Sidebar;
