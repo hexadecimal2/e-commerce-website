10708bed4941c52895809e04dcdd8fda5630dd89
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2024-07-17 01:39:39 +0200

    can be removed

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import '../App.css'
 const Bag = () => {
     return (
-        <>
+        <div className='vert-line'>
         {/* Palesa's section */}
          <div>Bag section</div>
-        </>
+         <div className='line'></div>
+        </div>
     );
 };
 
