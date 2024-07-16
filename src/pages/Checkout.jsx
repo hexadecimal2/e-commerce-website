e5b967b8424dcbdad1cc81b82acc101b879d68e1
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-16 02:31:44 +0200

    checkout page

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import OrderSummary from '../components/OrderSummary';
+ 
+
+const Checkout = () => {
+    return (
+        <>
+        {/* Neos section */}
+        {/* Take information from shipping address user input */}
+        {/* Take information from add payment user input */}
+        Review Your Bag Section
+        <OrderSummary />
+        </>
+    );
+};
+
+export default Checkout;
