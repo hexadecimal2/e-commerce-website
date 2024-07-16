4d53a03295605c1c08c8f921669191078900db02
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-16 02:32:29 +0200

    add address page

---
+++ src/pages/AddAddress.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddAddress = () => {
+    return (
+        <>
+         {/* Someone's section */}
+         Form for adding address
+        </>
+    );
+};
+
+export default AddAddress;
