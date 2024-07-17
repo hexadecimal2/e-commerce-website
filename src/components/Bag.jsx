80f98f2b2d6b8e5673f1ab39ccb69508f841b662
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2024-07-17 08:22:42 +0200

    change(s) undo=1;

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import '../App.css'
 const Bag = () => {
     return (
-        <div className='vert-line'>
+        <>
         {/* Palesa's section */}
          <div>Bag section</div>
-         <div className='line'></div>
-        </div>
+        </>
     );
 };
 
